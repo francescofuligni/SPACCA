d7b5567d199799c80c413cb2a7fa641ae0b1e932
cristianbaldari <cricco719@gmail.com>
2023-10-30 13:08:48 +0100

    Aggiunta board2player

--- GiocoSPACCA/src/application/Admin/CreatePlayerController.java
+++ GiocoSPACCA/src/application/Admin/CreatePlayerController.java
@@ -50,7 +50,7 @@ public class CreatePlayerController {
 	public void createPlayer(ActionEvent event) throws IOException {
 		String username = usernameField.getText();
 		
-		if(username==null || username.trim().equals("")) {			// controllo che non abbia nome vuoto
+		if(username==null || username.trim().equals("")) {			// controllare
 			Alert alert = new Alert(AlertType.ERROR);
 			alert.setTitle("Errore");
 			alert.setHeaderText("Errore nella creazione del giocatore");
@@ -59,42 +59,21 @@ public class CreatePlayerController {
 			message.setTextFill(Color.RED);
 			message.setText("Giocatore non aggiunto");
 			
-		} else if(username.contains(",")){
-			Alert alert = new Alert(AlertType.ERROR);
-			alert.setTitle("Errore");
-			alert.setHeaderText("Errore nella creazione del giocatore");
-			alert.setContentText("Non è possibile creare un giocatore con una virgola nel nome");
-			alert.showAndWait();
-			message.setTextFill(Color.RED);
-			message.setText("Giocatore non aggiunto");
-		}else {
-			Player player = new Player(username.trim());
-			if(!player.exists()) {								//controllo con metodo exists se é gia presente
-				Alert alert = new Alert(AlertType.CONFIRMATION);
-				alert.setTitle("Crea giocatore");
-				alert.setHeaderText("Stai creando un nuovo giocatore");
-				alert.setContentText("Sei sicuro di voler creare il giocatore \"" + username.trim() + "\"?");
+		} else {
+			Alert alert = new Alert(AlertType.CONFIRMATION);
+			alert.setTitle("Crea giocatore");
+			alert.setHeaderText("Stai creando un nuovo giocatore");
+			alert.setContentText("Sei sicuro di voler creare il giocatore \"" + username.trim() + "\"?");
 			
-				if(alert.showAndWait().get() == ButtonType.OK) {
+			if(alert.showAndWait().get() == ButtonType.OK) {
+				Player player = new Player(username.trim());
+				player.memorize();
 				
-					player.memorize();
-				
-					message.setTextFill(Color.GREEN);
-					message.setText("Giocatore \"" + username.trim() + "\" aggiunto correttamente");
-				} else {
-					message.setTextFill(Color.RED);
-					message.setText("Giocatore non aggiunto");
-				}
-			}
-			else {
-				Alert alert = new Alert(AlertType.ERROR);
-				alert.setTitle("Errore");
-				alert.setHeaderText("Errore nella creazione del giocatore");
-				alert.setContentText("Giocatore giá presente sul registro");
-				alert.showAndWait();
+				message.setTextFill(Color.GREEN);
+				message.setText("Giocatore \"" + username.trim() + "\" aggiunto correttamente");
+			} else {
 				message.setTextFill(Color.RED);
 				message.setText("Giocatore non aggiunto");
-				
 			}
 		}
 		usernameField.setText(null);
---
+++ GiocoSPACCA/src/application/SingleGame/Board2PlayersController.java
@@ -0,0 +1,44 @@
+package application.SingleGame;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.image.ImageView;
+
+public class Board2PlayersController {
+
+	@FXML
+	private Label currentPlayer;
+	
+	@FXML
+	private Label nextPlayer;
+	
+	@FXML
+	private ImageView card1;
+	
+	@FXML
+	private ImageView card2;
+	
+	@FXML
+	private ImageView card3;
+	
+	@FXML
+	private ImageView card4;
+	
+	@FXML
+	private ImageView card5;
+	
+	@FXML
+	private Button saveAndExit;
+	
+	@FXML
+	private ProgressBar lifeBar;
+	
+	@FXML
+	private Label lifePoints;
+	
+	
+	
+	
+}
