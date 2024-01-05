52bc90a3ba39ceb19ea2968661195fd282090d01
Francesco <114609462+francescofuligni@users.noreply.github.com>
2024-01-05 16:35:50 +0100

    errori in board

--- GiocoSPACCA/src/application/Play/BoardController.java
+++ GiocoSPACCA/src/application/Play/BoardController.java
@@ -11,15 +11,20 @@ import application.Card.Card;
 import application.Games.*;
 import application.MainMenu.MainMenuController;
 import application.Player.PlayerInGame;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 
 
@@ -28,6 +33,8 @@ public class BoardController implements Initializable{
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
+	private ArrayList<ImageView> images;
+	private ArrayList<Card> cards;
 	
 	@FXML
 	private Label currentPlayer, nextPlayer, healthPoints;
@@ -40,8 +47,11 @@ public class BoardController implements Initializable{
 	
 	@FXML
 	private ProgressBar healthBar;
+	
+	@FXML
+    private Button playCardButton;
 
-	Game game = new SingleGame(InsertCodeController.file);
+	private Game game = new SingleGame(InsertCodeController.file);
 		
 	private PlayerInGame current = game.currentPlayer();
 	private PlayerInGame next = game.nextPlayer();
@@ -56,22 +66,48 @@ public class BoardController implements Initializable{
 		healthBar.setStyle("-fx-accent: red;");							// healthBar rossa
 		healthBar.setProgress(current.getHealthPoints()/current.MAXHP);
 		
-		ArrayList<Card> cards = current.getHand();
-		ArrayList<ImageView> images = new ArrayList<>();
+		cards = current.getHand();
+		images = new ArrayList<>();
 		for(Card c : cards) {
 			ImageView iv = new ImageView();
 			iv.setImage(c.getPicture());
-			iv.setFitWidth(160);
+			iv.setFitWidth(150);
 			iv.setPreserveRatio(true);
 			images.add(iv);
 		}
 		hand.getItems().addAll(images);
 	}
 	
+	@FXML
+	public void playCard(ActionEvent e) {
+		
+		hand.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<ImageView>() {
+
+			@Override
+			public void changed(ObservableValue<? extends ImageView> arg0, ImageView arg1, ImageView arg2) {
+				
+				ImageView selected = hand.getSelectionModel().getSelectedItem();
+				
+				if(selected == null) {
+					Alert noCardSelected = new Alert(AlertType.ERROR);
+					noCardSelected.setTitle("ERRORE!");
+					noCardSelected.setHeaderText("ERRORE: SELEZIONA UNA CARTA");
+					noCardSelected.setContentText("Prima di giocare devi selezionare una carta");
+					noCardSelected.showAndWait();
+				} else {
+					Card c = cards.get(images.indexOf(selected));
+					c.effect(game);
+					game.nextTurn();
+					initialize(null, null);
+				}
+			}
+		});
+	}
+	
 	
 	
 	@FXML
-	public void save() throws IOException {
+	public void save(ActionEvent e) throws IOException {
 		game.save();
 		stage = (Stage)(saveAndExit.getScene().getWindow());
 		  //IMPORTANTE RICORDA IL ../ nell'URL DEL FXML
