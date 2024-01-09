d0b1deec23c721b11dc874a36a63235bd510e34a
cristianbaldari <cricco719@gmail.com>
2024-01-09 11:48:59 +0100

    cri

---
+++ GiocoSPACCA/src/application/Games/SimpleTournament.java
@@ -0,0 +1,24 @@
+package application.Games;
+
+import java.io.File;
+
+public class SimpleTournament extends Game {
+
+	public SimpleTournament(File game) {
+		super(game);
+		
+	}
+
+	@Override
+	public void removePlayer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void save() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
